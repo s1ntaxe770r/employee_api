e8abc14b9a9bf51e017be49e20aea7a9050868b2
s1ntaxe770r <cyberdev@protonmail.com>
2020-06-24 02:35:36 +0100

    change

--- emp/settings.py
+++ emp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['micro-managed.herokuapp.com','127.0.0.1','https://morning-caverns-24999.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 
